372f2cd6440522a4ee1a2afd2493274177c0a671
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-02-19 07:58:54 +0530

    settings.py change

--- vividvault/settings.py
+++ vividvault/settings.py
@@ -104,11 +104,11 @@ if ENVIRONMENT == 'production':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT', cast=int),
+            'NAME': config('DB_NAME', default=''),
+            'USER': config('DB_USER', default=''),
+            'PASSWORD': config('DB_PASSWORD', default=''),
+            'HOST': config('DB_HOST', default=''),
+            'PORT': config('DB_PORT', default='5432'),
         }
     }
 else:
