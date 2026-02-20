0b66729b491c9455fae2ff10f835c7dc1815afb1
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-02-20 16:29:46 +0530

    removed sqllit3

--- vividvault/settings.py
+++ vividvault/settings.py
@@ -96,24 +96,18 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database Configuration
 import os
 
-if os.environ.get("ENVIRONMENT") == "production":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ.get('RDS_PORT', '5432'),
-        }
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT', '5432'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 
