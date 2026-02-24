a4a8b2a0c59cc7fdd7c859ff6e052da45d42851e
Nishant Mishra <nishant.219302089@muj.manipal.edu>
2026-02-24 15:59:51 +0530

    media settings added

--- vividvault/settings.py
+++ vividvault/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vividvaultrds',
         'USER': 'vividuser',
-        'PASSWORD': config(PASSWORD),
+        'PASSWORD': config("PASSWORD"),
         'HOST': 'database-vivid.c1a26qeisten.ap-south-1.rds.amazonaws.com',
         'PORT': 5432,
         
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
+"""
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -155,18 +155,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
  ]
 
-
-# AWS S3 Static Files Configuration
 """
+# AWS S3 Static Files Configuration
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.ap-south-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
@@ -176,7 +176,9 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'vividvault.media_storages.MediaStorage'
-"""
+
+
+
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
